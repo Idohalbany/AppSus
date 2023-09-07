623cb354216b5b2fc35d7bd5c2c2b1174c6504cd
Ido Halbany <idohalbany97@gmail.com>
2023-09-07 14:30:23 +0300

    fixed mail bugs and added design and functions

--- apps/mail/cmps/MailPrivew.jsx
+++ apps/mail/cmps/MailPrivew.jsx
@@ -1,10 +1,11 @@
 import { LongTxt } from '../../../cmps/LongTxt.jsx'
 const { Link } = ReactRouterDOM
+const { useState } = React
 
 export function MailPrivew({ email, onDeleteEmail, onMarkEmail, onSetIsStarred }) {
   const { id, isRead, isStarred, subject, body, sentAt, removedAt, to, from, status, labels } =
     email
-
+  const [isHovered, setIsHovered] = useState(false)
   const grayBg = isRead ? 'whitesmoke' : 'white'
   const starBg = isStarred ? '#FFD700' : '#e8e8e8f3'
 
@@ -23,7 +24,11 @@ export function MailPrivew({ email, onDeleteEmail, onMarkEmail, onSetIsStarred }
   }
 
   return (
-    <div className='email-preview' style={{ backgroundColor: grayBg }}>
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className='email-preview'
+      style={{ backgroundColor: grayBg }}>
       {/* Checkbox */}
       <input type='checkbox' className='mail-checkbox' />
 
@@ -44,17 +49,20 @@ export function MailPrivew({ email, onDeleteEmail, onMarkEmail, onSetIsStarred }
         </div>
       </Link>
 
-      {/* Date */}
-      <span className='email-date'>{getSentDate(sentAt)}</span>
-
       {/* Icons */}
       <div className='icon-actions'>
-        <button onClick={(e) => handleButtonClick(e, 'delete')} className='trash-btn'>
-          <i className='fa fa-trash'></i>
-        </button>
-        <button onClick={(e) => handleButtonClick(e, 'mark')} className='read-btn'>
-          <i className={isRead ? 'fa fa-envelope-open' : 'fa fa-envelope'}></i>
-        </button>
+        {isHovered ? (
+          <React.Fragment>
+            <button onClick={(e) => handleButtonClick(e, 'delete')} className='trash-btn'>
+              <i className='fa fa-trash'></i>
+            </button>
+            <button onClick={(e) => handleButtonClick(e, 'mark')} className='read-btn'>
+              <i className={isRead ? 'fa fa-envelope-open' : 'fa fa-envelope'}></i>
+            </button>
+          </React.Fragment>
+        ) : (
+          <span className='email-date'>{getSentDate(sentAt)}</span>
+        )}
       </div>
     </div>
   )
